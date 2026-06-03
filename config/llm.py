9bfb01f12a13ec8a68319f4cd7ea9fc881bd3156
zar12321 <ldruszardii@gmail.com>
2026-06-03 23:42:59 +0700

    ganti jadi lite

--- config/llm.py
+++ config/llm.py
@@ -2,7 +2,7 @@ from langchain_google_genai import GoogleGenerativeAI
 
 def load_llm(api_key):
     llm = GoogleGenerativeAI(
-        model = 'gemini-2.5-flash', 
+        model = 'gemini-2.5-flash-lite', 
         api_key = api_key
     )
 
