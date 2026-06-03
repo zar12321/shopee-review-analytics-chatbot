7beedd0407cb8fdeac90dd13793507e9e1a33663
zar12321 <ldruszardii@gmail.com>
2026-06-04 00:00:44 +0700

    ganti dari flash lite jadi pro

--- config/llm.py
+++ config/llm.py
@@ -2,7 +2,7 @@ from langchain_google_genai import GoogleGenerativeAI
 
 def load_llm(api_key):
     llm = GoogleGenerativeAI(
-        model = 'gemini-2.5-flash-lite', 
+        model = 'gemini-2.5-pro', 
         api_key = api_key
     )
 
